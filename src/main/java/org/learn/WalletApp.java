674cf23d9802cf9099b89f452b32e0eae1f3e016
mikedr <derosamiguel@gmail.com>
2022-01-24 23:58:12 -0300

    First commit

---
+++ src/main/java/org/learn/WalletApp.java
@@ -0,0 +1,21 @@
+package org.learn;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class WalletApp {
+
+	public static void main(String[] args) {
+		
+//		Paso 2: Crear un Spring container
+		ClassPathXmlApplicationContext context  = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+//		Paso 3: Sacar los beans del Spring container
+		Funder funder = context.getBean("myFund",DebitCard.class);
+		
+		System.out.println(funder.fund());
+		
+		context.close();
+	}
+}
