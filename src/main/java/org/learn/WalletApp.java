52940157380c32708d97a7dac4ebc03164188015
mikedr <derosamiguel@gmail.com>
2022-06-26 15:56:15 -0300

    Update

--- src/main/java/org/learn/WalletApp.java
+++ src/main/java/org/learn/WalletApp.java
@@ -2,20 +2,15 @@ package org.learn;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 public class WalletApp {
 
 	public static void main(String[] args) {
-		
-//		Paso 2: Crear un Spring container
+		//Paso 2: Crear un Spring container
 		ClassPathXmlApplicationContext context  = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
-		
-//		Paso 3: Sacar los beans del Spring container
+		//Paso 3: Sacar los beans del Spring container
 		Funder funder = context.getBean("myFund", Funder.class);
-		
-		System.out.println(funder.fund());
-		
+		System.out.println("La wallet se fondeo con: $"+funder.fund());
 		context.close();
 	}
 }
